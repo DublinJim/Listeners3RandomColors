d74fbd7fa4e79cf63602e288c325d874e87779d2
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-24 11:18:17 -0500

    colours random

--- src/main/java/com/example/listeners3randomcolors/RandomColorController.java
+++ src/main/java/com/example/listeners3randomcolors/RandomColorController.java
@@ -1,6 +1,8 @@
 package com.example.listeners3randomcolors;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -8,23 +10,30 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 public class RandomColorController implements Initializable {
-private final Random random = new Random();
-
+    private final Random random = new Random();
+    public Button btnChangeBackground;
 
     public AnchorPane rootPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-changeColor();
     }
 
-    private void changeColor()
-    {
-        int red= random.nextInt();
-        int green= random.nextInt();
-        int blue= random.nextInt();
+    @FXML
+    private void changeColor() {
 
-        rootPane.setStyle("-fx-background-color: rgb("+red+","+green+","+blue+");");
+        int red = random.nextInt();
+
+        int green = random.nextInt();
+
+        int blue = random.nextInt();
+
+        rootPane.setStyle("-fx-background-color: rgb(" + red + "," + green + "," + blue + ");");
+        System.out.println("[---------------Colors----------------]");
+        System.out.println(" Blue is " + blue);
+        System.out.println(" Red is " + red);
+        System.out.println(" Green is " + green);
+        System.out.println("[---------------^^^^^^----------------]");
     }
 }
