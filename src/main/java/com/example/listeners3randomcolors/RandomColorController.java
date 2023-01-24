3943e1a111596cd1814daed1c6a6fcc333840ad2
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-24 11:22:54 -0500

    colours random in 256 range

--- src/main/java/com/example/listeners3randomcolors/RandomColorController.java
+++ src/main/java/com/example/listeners3randomcolors/RandomColorController.java
@@ -22,14 +22,12 @@ public class RandomColorController implements Initializable {
 
     @FXML
     private void changeColor() {
-
-        int red = random.nextInt();
-
-        int green = random.nextInt();
-
-        int blue = random.nextInt();
+        int red = random.nextInt(256);
+        int green = random.nextInt(256);
+        int blue = random.nextInt(256);
 
         rootPane.setStyle("-fx-background-color: rgb(" + red + "," + green + "," + blue + ");");
+
         System.out.println("[---------------Colors----------------]");
         System.out.println(" Blue is " + blue);
         System.out.println(" Red is " + red);
