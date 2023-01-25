d910dcad2a4edc7780d657124d7102ef90008938
DublinJim <dublinjim.jk@gmail.com>
2023-01-25 15:30:56 -0500

    optimised

--- src/main/java/com/example/listeners3randomcolors/RGBMaker.java
+++ src/main/java/com/example/listeners3randomcolors/RGBMaker.java
@@ -9,20 +9,12 @@ public class RGBMaker {
 
 
     private final Random random = new Random();
-    private ArrayList<Integer> rgb = new ArrayList<>();
-
-    private int red;
-    private int green;
-    private int blue;
-
-    private float redFloat;
-    private float greenFloat;
-    private float blueFloat;
+    private final ArrayList<Integer> rgb = new ArrayList<>();
 
     public ArrayList<Integer> getRgb() {
-        red = random.nextInt(256) + 1;
-        green = random.nextInt(256) + 1;
-        blue = random.nextInt(256) + 1;
+        int red = random.nextInt(256) + 1;
+        int green = random.nextInt(256) + 1;
+        int blue = random.nextInt(256) + 1;
         rgb.add(red);
         rgb.add(green);
         rgb.add(blue);
@@ -32,16 +24,11 @@ public class RGBMaker {
 
     public Color getColor() {
 
+        double redDouble = random.nextDouble();
+        double greenDouble = random.nextDouble();
+        double blueDouble = random.nextDouble();
 
-        double redDouble=random.nextDouble();
-        double greenDouble=random.nextDouble();
-        double blueDouble =random.nextDouble();
-
-
-        Color color = new Color(redDouble, greenDouble, blueDouble, .99);
-
-
-        return color;
+        return new Color(redDouble, greenDouble, blueDouble, .99);
     }
 
 }
